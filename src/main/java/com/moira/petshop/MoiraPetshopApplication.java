c48511fa9f1005dddb7c17f0364339ea34eb760a
Sam-TN <meysamtaherinia76@gmail.com>
2025-12-09 19:26:18 +0330

    Initial commit

---
+++ src/main/java/com/moira/petshop/MoiraPetshopApplication.java
@@ -0,0 +1,13 @@
+package com.moira.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiraPetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoiraPetshopApplication.class, args);
+	}
+
+}
